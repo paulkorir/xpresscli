bae537f37fec6d7e6c7129ecd9816c2871517f3e
paulkorir <paul@sci2pro.com>
2023-12-29 23:15:04 +0000

    now using CLIParser class which handles everything

--- xpresscli/experiment.py
+++ xpresscli/experiment.py
@@ -7,87 +7,58 @@ import sys
 import unittest
 
 
-class CLIParser(argparse.ArgumentParser):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.subparsers = None
-        self.manager = None
-
-
-def create_parser(json_specs):
-    """
-    Create an ArgumentParser with the desired arguments.
-
-    Returns:
-        CLIParser: Configured argument parser.
-    """
-    specs = json.loads(json_specs)
-    parser = CLIParser(**specs['parser'])
-    if 'subparser' in specs:
-        if specs['subparser'] is not None:
-            parser.subparsers = parser.add_subparsers(**specs['subparser'])
-            # process options
-            if not specs['subparser']['required']:
-                parser = create_parser_arguments(parser, specs['options'])
-            else:
-                if 'options' in specs:
-                    print(f"warning: options ignored for required subparser", file=sys.stderr)
-    return parser
-
-
-def create_parser_arguments(parser, json_specs):
-    """
-    Create an ArgumentParser based on the provided JSON specifications.
-
-    Args:
-        parser (argparse.ArgumentParser): parser to add the arguments to.
-        json_specs (str): JSON string containing the argument specifications.
-
-    Returns:
-        argparse.ArgumentParser: Configured argument parser.
-    """
-    if isinstance(json_specs, str):
-        specs = json.loads(json_specs)
-    elif isinstance(json_specs, list):
-        specs = json_specs
+def parse_options(parser, options):
+    """Parse the options"""
+    if isinstance(options, str):
+        specs = json.loads(options)
+    elif isinstance(options, list):
+        specs = options
     else:
-        raise TypeError(f"json_specs must be a string or list, not {type(json_specs)}")
-
+        raise TypeError(f"json_specs must be a string or list, not {type(options)}")
     for arg_spec in specs:
         # Add the argument to the parser
         flag = arg_spec.pop('flag')
         parser.add_argument(*flag, **arg_spec)
-    return parser
 
 
-def create_commands(parser, json_specs):
-    """
-    Create a command for a subparser.
+class CLIParser(argparse.ArgumentParser):
 
-    Args:
-        parser (CLIParser): parser to add the arguments to.
-        json_specs (str): JSON string containing the argument specifications.
+    def __init__(self, parser_spec: dict):
+        self._parser_spec = parser_spec.get('parser')
+        self._subparsers_spec = self._parser_spec.pop('subparsers', None)
+        self._options = self._parser_spec.pop('options', None)
+        super().__init__(**self._parser_spec)
+        # if none of the subparsers are required then we can add the options
+        self.managers = dict()
+        # self.subparsers = CLISubParsers(self, **self._subparsers_spec)
+        # what do I want from the subparsers?
+        # 2. add a subparser to the subparser to some maximum depth
+        # 3. add the commands to the subparser
+        self.subparsers = self._parse_subparsers(self._subparsers_spec)
+        # prepare the parser
+        parse_options(self, self._options)
 
-    Returns:
-        CLIParser: Configured argument parser.
-    """
-    assert parser.subparsers is not None, "Parser must have subparsers"
-    specs = json.loads(json_specs)
+    def _parse_subparsers(self, subparsers_spec):
+        if subparsers_spec is not None:
+            commands = subparsers_spec.pop('commands', None)
+            # todo: make it possible for a subparser to have a subparser
+            _subparser = subparsers_spec.pop('subparsers', None)
+            subparsers = self.add_subparsers(
+                **subparsers_spec,
+                parser_class=argparse.ArgumentParser
+            )
+            # add the commands
+            for command in commands:
+                options = command.pop('options', None)
+                manager_string = command.pop('manager', None)
+                self.managers[command['name']] = Manager(manager_string)
+                command_parser = subparsers.add_parser(**command)
+                if options is not None:
+                    parse_options(command_parser, options)
+            return subparsers
 
-    # we have to think of a manager as a diction of commands to callables
-    # the only problem with a plain dict is that we can't do any validation
-    managers = dict()
-    for command_spec in specs['commands']:
-        # Create the subparser
-        options = command_spec.pop('options')
-        manager_string = command_spec.pop('manager', None)
-        managers[command_spec['name']] = Manager(manager_string)
-        command_parser = parser.subparsers.add_parser(**command_spec)
-
-        create_parser_arguments(command_parser, options)
-
-    return managers
+    def __str__(self):
+        return self.format_help()
 
 
 def command_manager(args: argparse.Namespace) -> int:
@@ -122,6 +93,9 @@ class Manager:
     def __call__(self, *args, **kwargs):
         return self.function(*args, **kwargs)
 
+    def __str__(self):
+        return f"{self._module}.{self._function}"
+
 
 class Client:
     def __init__(self, parser_file='cli.json'):
@@ -148,65 +122,101 @@ if __name__ == '__main__':
 # unittests
 class Tests(unittest.TestCase):
     def setUp(self):
-        self.parser_specs = """
+        self.parser_spec = """
+{
+  "parser": {
+    "prog": "test",
+    "description": "Custom script with dynamic arguments",
+    "add_help": true,
+    "subparsers": {
+      "title": "subcommands",
+      "description": "valid subcommands",
+      "dest": "subcommand",
+      "required": true,
+      "subparsers": null,
+      "commands": [
         {
-            "parser": {
-                "prog": "test",
-                "description": "Custom script with dynamic arguments",
-                "add_help": true
-            },
-            "subparser": {
-                "title": "subcommands",
-                "description": "valid subcommands",
-                "dest": "subcommand",
-                "required": false
-            },
-            "options": [
-                {"flag": ["-x"], "help": "Path to the output file"},
-                {"flag": ["-w"], "help": "Enable verbose mode", "action": "store_true"}
-            ],
-            "commands": [
+          "name": "command",
+          "help": "command help",
+          "options": [
             {
-                "name": "command",
-                "help": "command help",
-                "options": [
-                    {"flag": ["input_file"], "help": "Path to the input file"},
-                    {"flag": ["-o"], "help": "Path to the output file"},
-                    {"flag": ["--verbose"], "help": "Enable verbose mode", "action": "store_true"}
-                ],
-                "manager": "experiment.command_manager"
+              "flag": [
+                "input_file"
+              ],
+              "help": "Path to the input file"
             },
             {
-                "name": "command2",
-                "help": "command2 help",
-                "options": [
-                    {"flag": ["input_file"], "help": "Path to the input file"},
-                    {"flag": ["-o"], "help": "Path to the output file"},
-                    {"flag": ["--verbose"], "help": "Enable verbose mode", "action": "store_true"}
-                ],
-                "manager": "experiment.command2_manager"
+              "flag": [
+                "-o"
+              ],
+              "help": "Path to the output file"
+            },
+            {
+              "flag": [
+                "--verbose"
+              ],
+              "help": "Enable verbose mode",
+              "action": "store_true"
             }
-            ]
+          ],
+          "manager": "experiment.command_manager"
+        },
+        {
+          "name": "command2",
+          "help": "command2 help",
+          "options": [
+            {
+              "flag": [
+                "input_file"
+              ],
+              "help": "Path to the input file"
+            },
+            {
+              "flag": [
+                "-o"
+              ],
+              "help": "Path to the output file"
+            },
+            {
+              "flag": [
+                "--verbose"
+              ],
+              "help": "Enable verbose mode",
+              "action": "store_true"
+            }
+          ],
+          "manager": "experiment.command2_manager"
         }
+      ]
+    },
+    "options": [
+      {
+        "flag": [
+          "-x"
+        ],
+        "help": "Path to the output file"
+      },
+      {
+        "flag": [
+          "-w"
+        ],
+        "help": "Enable verbose mode",
+        "action": "store_true"
+      }
+    ]
+  }
+}
         """
 
     def test_create_parser(self):
         """
         Test the example provided in the question.
         """
-        parser = create_parser(self.parser_specs)
-        sys.argv = shlex.split('script.py -x 37 -w')
-        print(parser)
-        args = parser.parse_args()
-        self.assertIsInstance(args, argparse.Namespace)
-
-    def test_create_parser_arguments(self):
-        """
-        Test the example provided in the question.
-        """
         # todo: there will have to be a way to prevent the wrong arguments from being passed; perhaps a schema enforced?
         sys.argv = shlex.split('script.py -x 37 -w')
-        parser = create_parser(self.parser_specs)
+        parser_spec = json.loads(self.parser_spec)
+        parser_spec['parser']['subparsers']['required'] = False
+        parser = CLIParser(parser_spec=parser_spec)
         args = parser.parse_args()
         self.assertIsInstance(args, argparse.Namespace)
         self.assertEqual('37', args.x)
@@ -216,23 +226,19 @@ class Tests(unittest.TestCase):
         """
         Test the example provided in the question.
         """
-        parser = create_parser(self.parser_specs)
-        print(f"{parser.subparsers = }")
+        parser = CLIParser(parser_spec=json.loads(self.parser_spec))
         self.assertIsInstance(parser, CLIParser)
         self.assertIsNotNone(parser.subparsers)
 
     def test_create_command(self):
         """Add a command to a subparser."""
-        parser = create_parser(self.parser_specs)
-        print(f"{parser._subparsers = }")
-        managers = create_commands(parser, self.parser_specs)
-        print(f"{managers = }")
+        parser = CLIParser(parser_spec=json.loads(self.parser_spec))
         sys.argv = shlex.split('script.py command input.txt -o output.txt --verbose')
         args = parser.parse_args()
-        print(f"{args = }")
+        self.assertEqual('command', args.subcommand)
         sys.argv = shlex.split('script.py command2 input.txt -o output.txt --verbose')
         args = parser.parse_args()
-        print(f"{args = }")
+        self.assertEqual('command2', args.subcommand)
 
     def test_manager_class(self):
         """Test that the manager attribute can be fired
@@ -247,8 +253,14 @@ class Tests(unittest.TestCase):
         # manager = Manager("experiment.command_manager")
         # exit_status = manager(args)
         sys.argv = shlex.split('script.py command input.txt -o output.txt --verbose')
-        manager = Manager("experiment.command_manager")
+        parser = CLIParser(parser_spec=json.loads(self.parser_spec))
+        args = parser.parse_args()
+        manager = parser.managers[args.subcommand]
         self.assertTrue(hasattr(manager, "module"))
         self.assertTrue(hasattr(manager, "function"))
         self.assertTrue(hasattr(manager, "__call__"))
         self.assertTrue(inspect.ismethod(getattr(manager, "__call__")))
+        self.assertEqual("experiment.command_manager", str(manager))
+        # trigger the manager
+        exit_status = manager(args)
+        self.assertEqual(0, exit_status)
